13a4f0c6a97487e6fd0b7f64452f61238773946b
Mahi1589 <mahananda152000@gmail.com>
2022-12-31 19:11:23 +0530

    Added code to Welcome Employee Wage

---
+++ src/com/empwagepractiseproblem/WelcomeEmpWage.java
@@ -0,0 +1,8 @@
+package com.empwagepractiseproblem;
+
+public class WelcomeEmpWage{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+}
+
